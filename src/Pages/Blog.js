dc5b66e7c3faf701597abb03d64688799667684e
Vitalize Business Solutions <jasonomalley1@hotmail.com>
2025-04-10 21:30:31 +0100

    Your commit message here

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -9,7 +9,7 @@ const Blog = () => {
       <ul>
         <li><strong>How Much Does a BER Cert Cost?</strong> – €120–€300 nationwide</li>
         <li><strong>BER Requirements When Selling</strong> – It’s a requirement in Ireland to have a BER assessment on homes being sold and rented.</li>
-        <li><strong>SEAI Grant Guide</strong> – What BER rating you need to qualify</li>
+        <li><strong>SEAI Grant Guide</strong> – What BER rating you nenped to qualify</li>
       </ul>
 
       <p>Check back soon as we update this section with more detailed posts and guides!</p>
